ed3f3d77d72e824bd0e5bb95af07c093c9297e6b
ynthia16 <c.umugwanez@alustudent.com>
2024-01-25 20:31:41 +0200

    This is the website i used tailwind in my styling

---
+++ Desktop/SheCanCode-practices4/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./html-folder/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
---
+++ Documents/Projects/alu-scripting-main/regular_expressions/README.md
@@ -0,0 +1,1 @@
+this is readme consist of regular expression syntax
